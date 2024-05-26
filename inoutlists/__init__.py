052c694a1f89782665ddc386a6f1df470cdd0072
ejtorre <ej.torre.nino@gmail.com>
2024-05-26 15:12:42 +0200

    init

--- inoutlists/__init__.py
+++
@@ -1,7 +0,0 @@
-from .loaders import load, Loader, LoaderXML, LoaderOFACXML, LoaderEUXML, LoaderUNXML
-from .dumpers import dump, Dumper, DumperJSON, DumperPandas, DumperCSV
-
-__all__ = [
-    "load", "Loader", "LoaderXML", "LoaderOFACXML", "LoaderEUXML", "LoaderUNXML",
-    "dump", "Dumper", "DumperJSON", "DumperPandas", "DumperCSV"
-]
